3708889c6cccc10084d913972749dd04b7df5ace
ParthipanSrinivasan <parthipanks2001@gmail.com>
2026-02-06 15:44:23 +0530

    hooks concept learning

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Hooks from './hooks/Hooks.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Hooks />
+  </StrictMode>
 )
