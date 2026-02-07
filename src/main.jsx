46838376c6e5580ab31ed3da2b30e1cc6631a9f7
ParthipanSrinivasan <parthipanks2001@gmail.com>
2026-02-07 12:50:07 +0530

    Learn hooks concepts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Hooks from './hooks/Hooks.jsx'
+import Header from './header/Header.jsx'
+import { BrowserRouter } from "react-router-dom"
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <Hooks />
-  </StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Header/>
+  </BrowserRouter>
+);
